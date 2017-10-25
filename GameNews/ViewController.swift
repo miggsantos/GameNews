14577a7c637c745885ed390ed90a769657e8ea5b
Miguel Santos <miggsantos@gmail.com>
2017-10-26 00:49:56 +0100

    Add project files

---
+++ GameNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GameNews
+//
+//  Created by Miguel Santos on 26/10/2017.
+//  Copyright © 2017 Miguel Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
